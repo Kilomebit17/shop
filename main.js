cd7c9e3b50d7f7c97b76c41deee27fc594c223dd
Oleg Stahniuk <oleh.stahnyuk@gmail.com>
2020-07-14 15:16:37 +0300

    delete slider

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
-const fordCar = 'Mersedess'
-const anotherCar = 'BMW'
-let car = [fordCar,2,14] //Запхали в массив переменную fordCar
-car[car.length] = anotherCar // продлили массив командой [car.length]
+// const fordCar = 'Mersedess'
+// const anotherCar = 'BMW'
+// let car = [fordCar,2,14] //Запхали в массив переменную fordCar
+// car[car.length] = anotherCar // продлили массив командой [car.length]
 
-// Циклы 
-for (let cars of car) { // изменили название переменной командой for (пишем переменною которою ми хотим сосдать "let cars" дальше от какой переменной будет взят масив of и названние переменно car) {}
-    console.log(cars) // дальше выводим в консоль ту переменную которою мы сосдали в for (let cars of car)
-}
-const person = new Object ({
-    height:180 + 'cm',
-    firstName: 'Vladilen',
-    lastName: 'Lamaiev',
-    languages:['ru,end,ua'],
-    isHasGirl:false
-})
-if (person.isHasGirl === false) {
-    console.log('Да так и есть!')
-}else {
-    console.log('Это не прада друг:)')
-}
-person.isHasGirl = true
-person.isShitWritter = false
-console.log(person)
-console.log(Math.pow(5,4))
+// // Циклы 
+// for (let cars of car) { // изменили название переменной командой for (пишем переменною которою ми хотим сосдать "let cars" дальше от какой переменной будет взят масив of и названние переменно car) {}
+//     console.log(cars) // дальше выводим в консоль ту переменную которою мы сосдали в for (let cars of car)
+// }
+// const person = new Object ({
+//     height:180 + 'cm',
+//     firstName: 'Vladilen',
+//     lastName: 'Lamaiev',
+//     languages:['ru,end,ua'],
+//     isHasGirl:false
+// })
+// if (person.isHasGirl === false) {
+//     console.log('Да так и есть!')
+// }else {
+//     console.log('Это не прада друг:)')
+// }
+// person.isHasGirl = true
+// person.isShitWritter = false
+// console.log(person)
+// console.log(Math.pow(5,4))
 
-const card = 5168757399933706
-if (card === 5168757399933706) {
-    console.log('Даннi введенно Правильно')
-}else {
-    console.log('Даннi введенно не правильно')
-}
+// const card = 5168757399933706
+// if (card === 5168757399933706) {
+//     console.log('Даннi введенно Правильно')
+// }else {
+//     console.log('Даннi введенно не правильно')
+// }
+
+// // number 
+// const someNumber = '42.42'
+// console.log(+someNumber + 2) 
+// console.log(Number.parseFloat(someNumber) + 2) 
+// console.log(parseFloat(someNumber) + 2)
+// console.log(parseFloat((0.4 + 0.2).toFixed(1))) 
+// const parser = 12
+// console.log(parseInt(parser) + 2)
+// console.log(Math.ceil(4.9)) // большую сторону
+// console.log(Math.floor(4.9)) // меньшую сторону
+// console.log(Math.round(4.5)) // 
+// console.log(Math.random().toFixed(1))
